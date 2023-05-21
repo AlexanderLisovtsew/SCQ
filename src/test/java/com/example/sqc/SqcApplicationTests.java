f8f0b5d372207cb2c8aae0e04c8a2691bf9ba20a
AlexanderLisovtsew <77970426+AliaksandrLsv@users.noreply.github.com>
2023-05-21 15:22:23 +0200

    added data

---
+++ src/test/java/com/example/sqc/SqcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
